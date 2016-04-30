7b2e357e326e8414b247242ed0d19be54ce4f462
Iceberg YOUNG <iceberg.young@gmail.com>
2016-04-30 09:02:51 +0800

    Moved from CMake to ninja.

--- example.cpp
+++ example.cpp
@@ -7,9 +7,9 @@ using namespace so;
 int main() {
     auto a = version::parse("123.456.789-hello.world.10+2015.3.1");
     auto b = version::parse("123.456.789-hello.world.3+2015.3.1");
-    cout << (a < b);
+    cout << boolalpha << (a < b);
 
-    cout << std::string(a + version::change::incompatible + version::change::fixed);
+    cout << std::string(a + version::change::incompatible + version::change::compatible);
     cout << (a != b) << ("0.0.0-x" == b);
     return 0;
 }
